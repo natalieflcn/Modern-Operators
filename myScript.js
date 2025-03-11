39c3beb412e728c66cfeaf87f09f58d0aff0636e
natalieflcn <natalie.dflcn@gmail.com>
2025-03-11 12:00:40 -0400

    Completed Coding Challenge 4

--- myScript.js
+++ myScript.js
@@ -199,3 +199,46 @@ console.log(
 for (const [key, value] of gameEvents) {
   console.log(`[${key < 45 ? 'FIRST' : 'SECOND'} HALF] ${key}: ${value}`);
 }
+
+// Coding Challenge #4
+console.log('CODING CHALLENGE 4 -- FINAL STRING CHALLENGE');
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+// Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  let count = 0;
+
+  for (const word of text.split('\n')) {
+    count++;
+    let [firstWord, secondWord] = word.toLowerCase().trim().split('_');
+    secondWord = secondWord.charAt(0).toUpperCase() + secondWord.slice(1);
+
+    console.log((firstWord + secondWord).padEnd(20, ' ') + '✅'.repeat(count));
+  }
+});
+
+// The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+// THIS TEST DATA (pasted to textarea)
+// underscore_case
+//  first_name
+// Some_Variable
+//   calculate_AGE
+// delayed_departure
+
+// SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+// underscoreCase      ✅
+// firstName           ✅✅
+// someVariable        ✅✅✅
+// calculateAge        ✅✅✅✅
+// delayedDeparture    ✅✅✅✅✅
+
+// HINT 1: Remember which character defines a new line in the textarea 😉
+// HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+// HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+// HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
